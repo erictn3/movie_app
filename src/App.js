ac83df90da45959ae5c81d6f5960738407b37a48
Eric Nguyen <erictn3@uci.edu>
2021-08-13 14:17:31 -0700

    utilizing useeffect and usestate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import React, {useEffect, useState} from "react";
 
 import Movie from './components/Movie';
 
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?api_key=a5ab91b6c0e1e8bf686fe28b77bb4282&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
+
+const API_Key = 'a5ab91b6c0e1e8bf686fe28b77bb4282'
+
+
 function App() {
-  const movies = ['1','2','3']
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API).then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setMovies(data);      
+    })
+  }, [])
+
     return (
       <div className="App">
-        {movies.map(movie=> (
+        {movies.length > 0 && movies.map((movie)=> (
           <Movie />
         ))}
 
